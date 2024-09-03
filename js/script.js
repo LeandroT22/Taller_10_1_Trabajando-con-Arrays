201c7a8fb6fff399966177e971107eb6e1a1760d
LeandroT22 <trucidoleandro@gmail.com>
2024-09-02 21:14:48 -0300

    Muestro los strings del arreglo (filtrados y ordenados a-z)

--- js/script.js
+++ js/script.js
@@ -39,4 +39,7 @@ function showList(array) {
 document.addEventListener("DOMContentLoaded", (e) => {
   // Escribe tu solución aquí
   // Sugerencia de cómo mostrar el array => showList(strangeArray);
+  let strings = strangeArray.filter(element => typeof element === 'string');
+  let stringsordenados = strings.sort((a, b) => a.localeCompare(b));
+  showList(stringsordenados);
 });
